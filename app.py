3439f028c751492aacd21e5d0105d2c0168f7819
jansensensensen <58451912+jansensensensen@users.noreply.github.com>
2024-12-10 15:20:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ if menu == "Overview":
 elif menu == "Data Visualization":
     st.header("Data Visualization")
     st.subheader("Count of Wine Quality")
-    fig, ax = plt.subplots(figsize=(10, 5))
+    fig, ax = plt.subplots(figsize=(20, 10))
     sns.countplot(x='quality', data=df, ax=ax)
     plt.title('Count of Wine Quality')
     plt.tight_layout()
